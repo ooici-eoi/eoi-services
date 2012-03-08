1e4fe4e1e6f6362711e358def61cf9b688902c03
Tim Giguere <tgiguere@asascience.com>
2012-03-08 13:23:33 -0500

    Using USGS constructor in Pyon to send USGS packets

--- eoi/agent/handler/usgs_stream_publisher.py
+++ eoi/agent/handler/usgs_stream_publisher.py
@@ -20,90 +20,10 @@ import time
 
 import random
 
-from prototype.sci_data.ctd_stream import ctd_stream_packet, ctd_stream_definition, usgs_stream_definition
+from prototype.sci_data.stream_defs import USGS_stream_definition
 from prototype.sci_data.constructor_apis import PointSupplementConstructor
 
 from interface.services.dm.ipubsub_management_service import PubsubManagementServiceClient
-
-class SimpleCtdPublisher(StandaloneProcess):
-    def __init__(self, *args, **kwargs):
-        super(StandaloneProcess, self).__init__(*args,**kwargs)
-        #@todo Init stuff
-
-    def on_start(self):
-        '''
-        Creates a publisher for each stream_id passed in as publish_streams
-        Creates an attribute with the name matching the stream name which corresponds to the publisher
-        ex: say we have publish_streams:{'output': my_output_stream_id }
-          then the instance has an attribute output which corresponds to the publisher for the stream
-          in my_output_stream_id
-        '''
-
-        # Get the stream(s)
-        stream_id = self.CFG.get('process',{}).get('stream_id','')
-
-        self.greenlet_queue = []
-
-        # Stream creation is done in SA, but to make the example go for demonstration create one here if it is not provided...
-        if not stream_id:
-
-            ctd_def = ctd_stream_definition(stream_id=stream_id)
-            pubsub_cli = PubsubManagementServiceClient(node=self.container.node)
-            stream_id = pubsub_cli.create_stream(
-                name='Example CTD Data',
-                stream_definition=ctd_def,
-                original=True,
-                encoding='ION R2')
-
-        self.stream_publisher_registrar = StreamPublisherRegistrar(process=self,node=self.container.node)
-        # Needed to get the originator's stream_id
-        self.stream_id= stream_id
-
-
-        self.publisher = self.stream_publisher_registrar.create_publisher(stream_id=stream_id)
-
-
-        self.last_time = 0
-
-
-        g = Greenlet(self._trigger_func, stream_id)
-        log.debug('Starting publisher thread for simple ctd data.')
-        g.start()
-        self.greenlet_queue.append(g)
-
-    def on_quit(self):
-        for greenlet in self.greenlet_queue:
-            greenlet.kill()
-        super(SimpleCtdPublisher,self).on_quit()
-
-
-    def _trigger_func(self, stream_id):
-
-        while True:
-
-            length = random.randint(1,20)
-
-            c = [random.uniform(0.0,75.0)  for i in xrange(length)]
-
-            t = [random.uniform(-1.7, 21.0) for i in xrange(length)]
-
-            p = [random.lognormvariate(1,2) for i in xrange(length)]
-
-            lat = [random.uniform(-90.0, 90.0) for i in xrange(length)]
-
-            lon = [random.uniform(0.0, 360.0) for i in xrange(length)]
-
-            tvar = [self.last_time + i for i in xrange(1,length+1)]
-
-            self.last_time = max(tvar)
-
-            ctd_packet = ctd_stream_packet(stream_id=stream_id,
-                c=c, t=t, p=p, lat=lat, lon=lon, time=tvar)
-
-            log.warn('SimpleCtdPublisher sending %d values!' % length)
-            self.publisher.publish(ctd_packet)
-
-            time.sleep(2.0)
 
 class UsgsPublisher(StandaloneProcess):
 
@@ -125,7 +45,7 @@ class UsgsPublisher(StandaloneProcess):
 
         self.greenlet_queue = []
 
-        self._usgs_def = usgs_stream_definition(stream_id=stream_id)
+        self._usgs_def = USGS_stream_definition()
 
         # Stream creation is done in SA, but to make the example go for demonstration create one here if it is not provided...
         if not stream_id:
